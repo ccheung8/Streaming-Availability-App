0197473a3bb0284acc7d836091f5a9fc67df50ff
ccheung8 <c4cheung@ucsd.edu>
2025-03-20 03:04:15 -0700

    created services page and displays result on h1 element

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const movieSearch = document.getElementById('movieSearch');
+// const movieSearch = document.getElementById('movieSearch');
 const movieInfo = [];
 
 class Movie {
@@ -13,45 +13,52 @@ class Movie {
   }
 }
 
-async function getMovie() {
-  let movieID = findMovie(movieInfo, movieSearch.value.toLowerCase());
-  if (movieID === -1) {
-    console.log("movie not found");
+/**
+ * @definition gets movie from watchmode API and displays result on page
+ * @param {string} movieSearch - name of movie to search for
+ */
+async function getMovie(movieSearch) {
+  // waits for csv to load to data to initialize
+  await loadMovies();
+  // finds index of movie
+  let movieIdx = findMovie(movieInfo, movieSearch.toLowerCase());
+  const servicesDiv = document.getElementById('servicesTitle');
+  if (movieIdx === -1) {
+    servicesDiv.innerText = "movie not found";
   } else {
-    // const movie = await axios.get("https://api.watchmode.com/v1/title/" + movieID + "/sources/?apiKey=QJ1dtRDuLGfv2iErp7XSEoQbymXut7IAagrANh88");
+    const movie = await axios.get("https://api.watchmode.com/v1/title/" + movieInfo[movieIdx].id + "/sources/?apiKey=QJ1dtRDuLGfv2iErp7XSEoQbymXut7IAagrANh88");
     // console.log(movie.data);
-    // if (movie.data.length) {
-    //   console.log("media is out on:");
-    //   for (let i = 0; i < movie.data.length; i++) {
-    //     if (movie.data[i].region === "US" && movie.data[i].type === "sub") {
-    //       console.log(`Film is available on ${movie.data[i].name}`);
-    //     }
-    //   }
-    // } else {
-    //   console.log("not on streaming");
-    // }
+    if (movie.data.length) {
+      servicesDiv.innerText = `${movieInfo[movieIdx].name} is available on:`;
+      for (let i = 0; i < movie.data.length; i++) {
+        if (movie.data[i].region === "US" && movie.data[i].type === "sub") {
+          // console.log(`Film is available on ${movie.data[i].name}`);
+          servicesDiv.innerText += ` ${movie.data[i].name}`;
+        }
+      }
+    } else {
+      servicesDiv.innerText = "not on streaming";
+    }
   }
 }
 
 /**
- * @definition linear search of movieInfo array for ID
+ * @definition linear search of movieInfo array for Index
  * @param {array} movieInfo - array of movie objects
  * @param {string} movieName - name of movie to find ID for
- * @returns ID of movie, else -1 if not found
+ * @returns Index of movie in array, else -1 if not found
  */
 function findMovie(movieInfo, movieName) {
   for (let i = 0; i < movieInfo.length; i++) {
     if (movieInfo[i].name === movieName) {
-      console.log(`${movieName} has WatchMode ID of ${movieInfo[i].id}`);
-      return movieInfo[i].id;
+      return i;
     }
   }
   return -1
 }
 
-// gets csv and arranges into an array on page load
-window.onload = async function() {
-  let res = await axios.get("title_id_map.csv");
+async function loadMovies() {
+  let res = await axios.get("../title_id_map.csv");
   // turns csv data into 2d array
   const dataArray = res.data.split("\n").map(row => row = row.split(
     // finds , that doesn't have " before and after 
